8b3905be497d991a696a2f21bc6d083523b44f17
bdas4018 <mailmebhargavdas@rediffmail.com>
2019-06-19 01:18:06 -0400

    Update test_wrap.go

--- test_wrap.go
+++ test_wrap.go
@@ -1,1 +1,176 @@
+    SigPicnicL1FS   
+     SigPicnicL1FS   
+    SigPicnicL1FS    
+   SigPicnicL1FS    
+  SigPicnicL1FS    
+   SigPicnicL1FS    
+  SigPicnicL1FS    
+  SigPicnicL1FS    
+  SigPicnicL1FS    
+  SigPicnicL1FS    
+  SigPicnicL1FS    
+  SigPicnicL1FS
+
+
+package goliboqs
+
+import (
+	"testing"
+  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const libPath = "/usr/local/liboqs/lib/liboqs.so"
+
+func TestRoundTrip(t *testing.T) {
+
+	kems := []KemType{
+		KemBike1L1,
+		KemBike1L3,
+		KemBike1L5,
+		KemBike2L1,
+		KemBike2L3,
+		KemBike2L5,
+		KemBike3L1,
+		KemBike3L3,
+		KemBike3L5,
+		KemFrodo640AES,
+		KemFrodo640Shake,
+		KemFrodo976AES,
+		KemFrodo976Shake,
+		KemFrodo1344AES,
+		KemFrodo1344Shake,
+		KemNewHope512,
+		KemNewHope1024,
+		KemKyber512,
+		KemKyber768,
+		KemKyber1024,
+		KemSidhP503,
+		KemSidhP751,
+		KemSikeP503,
+		KemSikeP751,
+	}
+
+	k, err := LoadLib(libPath)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, k.Close()) }()
+
+	for _, sigAlg := range sigs {
+		t.Run(string(sigAlg), func(t *testing.T) {
+			//t.Parallel() <-- cannot use this because https://github.com/stretchr/testify/issues/187
+
+			testSIG, err := k.GetSig(sigAlg)
+			if err == errAlgDisabledOrUnknown {
+				t.Skipf("Skipping disabled/unknown algorithm %q", sigAlg)
+			}
+			require.NoError(t, err)
+			defer func() { require.NoError(t, testSIG.Close()) }()
+
+			publicKey, secretKey, err := testSIG.KeyPair()
+			require.NoError(t, err)
+
+			sharedSecret, ciphertext, err := testSIG.Encaps(publicKey)
+			require.NoError(t, err)
+
+			recoveredSecret, err := testSIG.Decaps(ciphertext, secretKey)
+			require.NoError(t, err)
+
+			assert.Equal(t, sharedSecret, recoveredSecret)
+		})
+	}
+}
+
+func TestBadLibrary(t *testing.T) {
+	_, err := LoadLib("bad")
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to load module")
+}
+
+func TestReEntrantLibrary(t *testing.T) {
+	k1, err := LoadLib(libPath)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, k1.Close()) }()
+
+	k2, err := LoadLib(libPath)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, k2.Close()) }()
+}
+
+func TestLibraryClosed(t *testing.T) {
+	k, err := LoadLib(libPath)
+	require.NoError(t, err)
+	require.NoError(t, k.Close())
+
+	const expectedMsg = "library closed"
+
+	t.Run("GetSIG", func(t *testing.T) {
+		_, err := k.GetSig(KemBike1L1)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), expectedMsg)
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		err := k.Close()
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), expectedMsg)
+	})
+}
+
+func TestKEMClosed(t *testing.T) {
+	k, err := LoadLib(libPath)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, k.Close()) }()
+
+	testKEM, err := k.GetSig(KemKyber512)
+	require.NoError(t, err)
+
+	require.NoError(t, testSIG.Close())
+
+	t.Run("KeyPair", func(t *testing.T) {
+		_, _, err := testSIG.KeyPair()
+		assert.Equal(t, errAlreadyClosed, err)
+	})
+
+	t.Run("Encaps", func(t *testing.T) {
+		_, _, err := testSIG.Encaps(nil)
+		assert.Equal(t, errAlreadyClosed, err)
+	})
+
+	t.Run("Decaps", func(t *testing.T) {
+		_, err := testSIG.Decaps(nil, nil)
+		assert.Equal(t, errAlreadyClosed, err)
+	})
+
+	t.Run("Decaps", func(t *testing.T) {
+		err := testSIG.Close()
+		assert.Equal(t, errAlreadyClosed, err)
+	})
+}
+
+func TestInvalidSIGAlg(t *testing.T) {
+	k, err := LoadLib(libPath)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, k.Close()) }()
+
+	_, err = k.GetSig(SigType("this will never be valid"))
+	assert.Equal(t, errAlgDisabledOrUnknown, err)
+}
+
+func TestLibErr(t *testing.T) {
+	// Difficult to test this without a deliberately failing KEM library (which could
+	// be a future idea...)
+
+	err := libError(operationFailed, "test%d", 123)
+	assert.EqualError(t, err, "test123")
+}
+
+
+
+	
+
+			
+
+	
+
+	
 
